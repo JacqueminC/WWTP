df20e981c272a1d299e76deeae20b6fbfe09b8c7
Cédric <cedjacq@outlook.com>
2021-05-09 17:12:19 +0200

    change test table

--- wwtp_app/table/model.py
+++ wwtp_app/table/model.py
@@ -2,18 +2,30 @@ from joueur.model import Joueur
 
 class Table:
     idTable = ""
-    jeuxLibre = ""
-    nbDePlaceLibre = 4
+    #jeuxLibre True pour jeux libre ou False pour jeux prédéfini
+    nbPlace = 4
     date = ""
     ville = ""
     ageMin = 18
     regle = False
     noteMin = 0
 
-    def __init__(self, hote , jeuxLibre, nbDePlaceLibre, jeux, date, ville, ageMin, regle, noteMin):
+    def __init__(self, hote , jeuxLibre, nbPlace, jeux, date, ville, ageMin, regle, noteMin):
         self.hote = hote
         self.jeuxLibre = jeuxLibre
-        self.nbDePlaceLibre = nbDePlaceLibre
+
+        if nbPlace < 1:
+            raise Exception("Il doit y avoir au moins une place de libre")
+        else:
+            self.nbPlace = nbPlace
+        
+        if jeuxLibre == True:
+            self.jeux = None
+        elif jeuxLibre == False and len(jeux) < 1:
+            self.jeuxLibre == True
+        else:
+            self.jeux = jeux
+
         self.jeux = jeux
         self.date = date
         self.ville = ville
--- wwtp_app/tests/test_table.py
+++ wwtp_app/tests/test_table.py
@@ -12,35 +12,37 @@ jeu1 = Jeu("Carcassonne", "")
 jeu2 = Jeu("Catane", "")
 jeu3 = Jeu("SmashUp", "")
 
-jeux = [jeu1, jeu2, jeu3]
+jeux1 = [jeu1, jeu2, jeu3]
+jeux2 = []
+jeux3 = None
 
 dateBefore1 = datetime.today() - timedelta(days=60)
 dateNow = datetime.today()
 dateAfter1 = datetime.today() + timedelta(days=60)
 
 
-def test_new_table():
-
-    table1 = Table(joueur1, False, 5, jeux, dateAfter1, "La comté", 18, False, False)
-    assert table1.jeuxLibre == False, "Jeux libre n'as pas été choisi"
-    assert table1.nbDePlaceLibre >= 1, "Il doit y avoir au moins une place de libre"
-    assert len(table1.jeux) >= 1, "Jeux libre n'est pas séléctionne la liste de jeu doit contenir au moins 1 jeu"
-    for j in jeux:
-        assert j.nom != None, "le nom du jeu ne peut pas être à None"
-        assert j.nom != "", "le nom du jeu ne peut pas être vide"
-        assert j.version != None, "la version du jeu ne peut pas être à None"
-    
-    assert table1.date >= datetime.today() + timedelta(hours=2), "La date de la table doit être à today() + 2h"
-    assert table1.ville != "", "La ville ne peut pas être vide"
-    
-    table2 = Table(joueur1, True, 7, [], dateAfter1, "La comté", 18, False, False)
-    assert table2.jeuxLibre == True, "Jeux libre as été choisi"
-    assert table2.nbDePlaceLibre >= 1, "Il doit y avoir au moins une place de libre"
-    assert len(table2.jeux) == 0, "Jeux libre est séléctionne la liste de jeu ne doit pas contenir de jeux"    
-    assert table2.date >= datetime.today() + timedelta(hours=2), "La date de la table doit être à today() + 2h"
-    assert table2.ville != "", "La ville ne peut pas être vide"
-
+def test_new_table(): 
     #TODO generate bad var for test
-    table3 = Table(joueur1, True, 7, jeux, dateNow, "La comté", 0, False, False)
-    table4 = Table(joueur1, True, 7, jeux, dateBefore1, "La comté", 0, False, False)
+
+    try:
+        table1 = Table(joueur1, False, 4, jeux1, dateAfter1, "La comté", 18, False, False)
+    except Exception as e:
+        pytest.fail("Aucune erreur ne doit être retourné - " + str(e))
+
+    
+    table1 = Table(joueur1, True, 5, jeux1, dateAfter1, "La comté", 18, False, False)
+    assert table1.jeux == None, "Jeux libre à été choisi, la liste ne doit pas être enregistrée"
+
+    table1 = Table(joueur1, False, 5, jeux2, dateAfter1, "La comté", 18, False, False)
+    assert table1.jeuxLibre == True, "Aucun jeu n'a été encodé la table doit passer en jeux libre"
+
+    with pytest.raises(Exception) as e:
+        table1 = Table(joueur1, False, 5, jeux2, dateNow, "La comté", 18, False, False)
+    assert "La date ne peut pas être antérieur à la date actuelle PLUS 2h" in str(e.value)
+
+    with pytest.raises(Exception) as e:
+        table1 = Table(joueur1, False, 5, jeux2, dateAfter1, "", 18, False, False)
+    assert "La ville ne peut pas être vide" in str(e.value)
+
+
 
