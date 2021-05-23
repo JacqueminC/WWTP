d0b2014903d24276ced20367b324147c6f014a28
Cédric <cedjacq@outlook.com>
2021-05-23 15:42:44 +0200

    validator form table

--- wwtp_app/table/table.py
+++ wwtp_app/table/table.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, app, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, FormField, FieldList
+from wtforms import validators
 from wtforms.fields.html5 import IntegerRangeField, DateTimeField, DateField, TimeField
 from wtforms.form import Form
-from wtforms.validators import DataRequired, InputRequired, NumberRange, ValidationError
+from wtforms.validators import InputRequired, ValidationError
+from datetime import datetime, time, timedelta
 
 bpTable = Blueprint("table", __name__, template_folder="templates")
 
@@ -15,40 +17,40 @@ class CreationTableForm(FlaskForm):
     jeuxLibre = BooleanField(' Jeux libre ?')
     nbPlace = IntegerField('Nombre de place disponnible', validators=[InputRequired()])
     jeux = FieldList(FormField(JeuxListeForm), min_entries=1)
-    date = DateField('Date', format='%Y-%m-%d')
-    heure = TimeField('Heure')
+    date = DateField('Date', format='%Y-%m-%d', validators=[InputRequired()])
+    heure = TimeField('Heure', validators=[InputRequired()])
     ville = StringField('Ville', validators=[InputRequired()])
     ageMin = BooleanField('Définir un âge minimum ?')
     age = IntegerField(' Age minimum ?', default=0)
     regle = BooleanField(' Connaissance des règles requises ?')
     noteMin = BooleanField(' Note minimum ?')
-    note = IntegerRangeField('Note')
+    note = IntegerRangeField('Note', default=0)
 
-    def validate(self):        
-        if not Form.validate(self):
-            return False
-        
-        if self.jeuxLibre.data == False and (self.jeux[0].nom.data == None or self.jeux[0].nom.data == ""):
-            print("jeux vide")
-            return False
-        
-        return True 
+    def validate_jeux(self, jeux):        
+        if self.jeuxLibre.data == False:
+            if self.jeux[0].nom.data == None or self.jeux[0].nom.data == "":
+                raise ValidationError("Vous devez indiquer un jeu si vous êtes pas en jeux libre!")
+     
 
     def validate_age(self, age):
         if self.ageMin.data == True:
-            print("check age value")
+            if not isinstance(self.age.data, int):
+                raise ValidationError("Veuillez entrez un age en chiffre!")
             if self.age.data == None:
-                raise ValidationError("Veuillez indiquer une valeur! L'age doit être entre 16 et 99")
+                raise ValidationError("Veuillez indiquer une valeur! L'age doit être entre 16 et 99!")
             if self.age.data < 16 or self.age.data >99:
-                raise ValidationError("L'age doit être entre 16 et 99")
-
-
+                raise ValidationError("L'age doit être entre 16 et 99!")
     
-        
-         
+    def validate_date(self, date):
+        now = datetime.now() + timedelta(hours=2)
+        print(now)
+        dtString = str(date.data) + " " + str(self.heure.data)
+        dtForm = datetime.strptime(dtString, '%Y-%m-%d %H:%M:%S')
 
-       
+        print("dtForm : " + str(dtForm))
 
+        if now > dtForm:
+            raise ValidationError("La date doit être supérieur à maintenant PLUS 2 heures!")
             
 
 @bpTable.route("/tableForm", methods=["GET", "POST"])
@@ -56,6 +58,6 @@ def creationTable():
     form = CreationTableForm()
 
     if form.validate_on_submit():
-        return "Form envoyé! {} and {}".format(form.nbPlace.data, form.ville.data)
+       return "Form envoyé! {} and {}".format(form.nbPlace.data, form.ville.data)
 
     return render_template("tableForm.html", form=form)
