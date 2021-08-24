96e8746e5b0e9dd0229d65f47eae85cbc4e3e6b8
JacqueminC <cedjacq@outlook.com>
2021-08-24 22:47:12 +0200

    test flask-pwa

--- wwtp_app/wwtp/app.py
+++ wwtp_app/wwtp/app.py
@@ -55,14 +55,6 @@ def index():
 
     return render_template("index.html")
 
-@app.route('/offline.html')
-def offline():
-    return app.send_static_file('offline.html')
-
-@app.route('/service-worker.js')
-def sw():
-    return app.send_static_file('service-worker.js')
-
 if __name__ == "__main__":
     app.run(debug=True)
     """/var/log/gunicorn/"""
